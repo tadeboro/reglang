88b5a718c7ba8805cf5a1ececa2b09f324507254
aliman <omerzel.vales@gmail.com>
2014-11-20 13:00:19 +0100

    Pretvori regIzraza v avtomat

--- src/Enumerator.hs
+++ src/Enumerator.hs
@@ -1,9 +1,13 @@
 module Enumerator 
 (
 -- * Tip
-Rexp
+Rexp,
+NFA,
 -- * Funkcija
---enumR
+enum,
+r2n,
+
+sandwich
 )
 where
 
@@ -18,6 +22,16 @@ data Rexp = Nil               -- empty language
           | Cat Rexp Rexp     -- catenation
           | Alt Rexp Rexp     -- alternation
             deriving (Show,Eq,Ord)
+
+type NFA = [State]
+data State = State Ident Char NFA deriving Show
+
+instance Eq State where
+    (State i _ _) == (State i' _ _) = i==i'
+instance Ord State where
+    (State i c _) <= (State i' c' _) = (c,i) <= (c',i')
+
+type Ident = Int
 
 -- | Length-ordered list
 -- Shorter lists come before longer lists when sorting			
@@ -60,7 +74,67 @@ cat = xprod (+++)
 clo :: [LOS] -> [LOS]
 clo = closure (+++) (LOL "")	
 	
+
+bp :: Bool -> NFA -> NFA
+bp True ds = ds
+bp False _ = []
+
+r2n :: Rexp -> NFA        
+r2n r = let {
+    ds = [State 0 '~' []];
+    (fs, _, b) = r2n' r 1 ds
+    } in fs \/ (bp b ds)
+
+r2n' :: Rexp -> Ident -> NFA -> (NFA,Ident,Bool)
+r2n' Nil n _ = ([], n, False)
+r2n' Eps n ds = ([], n, True)
+r2n' (Sym c) n ds = ([State n c ds], succ n, False)
+r2n' (Cat x y) n ds = let {
+    (fs, n', b) = r2n' y n ds;
+    (fs', n'', b') = r2n' x n' (fs\/(bp b ds));
+    } in (fs'\/(bp b' fs), n'', b&&b')
+r2n' (Alt x y) n ds = let {
+    (fs, n', b) = r2n' y n ds;
+    (fs', n'', b') = r2n' x n' ds;
+    } in (fs\/fs', n'', b||b')
+r2n' (Clo x) n ds = let {
+    (fs, n', b) = r2n' x n (fs\/ds)
+    } in (fs, n', True)
 	
+
+grp :: NFA -> NFA
+grp (m@(State _ c ds) : ms@((State _ c' ds'):mt)) =
+    if c==c' then grp ((State (-1) c (ds\/ds')):mt)
+    else m : grp ms
+grp ms = ms
+
+accept :: NFA -> Bool
+accept ds = 0 `elem` [i | (State i _ _) <- ds]
+
+visit :: [(String,NFA)] -> [String]     
+visit [] = []
+visit ((x,ds):ws) =
+    let xs = visit (ws ++ [(x++[c],ds') | (State _ c ds') <- grp ds])
+    in if accept ds then x:xs else xs
+
+
+enum :: NFA -> [String]
+enum starts = visit [("",starts)]
+	
+	
+
+a = Sym 'a'
+b = Sym 'b'
+c = Sym 'c'
+aa = Cat a a
+bb = Cat b b
+ab = Cat a b
+ba = Cat b a
+a_a = Alt a a
+b_b = Alt b b
+a_b = Alt a b	
+	
+sandwich = Cat a (Cat (Clo b) a)
 --enumR :: Rexp -> [String]
 --enumR r = undefined
 
